75ec254f0294af127dedcb2e2c751ff0a4d6d29b
ehsan <ehsanchhry@gmail.com>
2020-05-06 15:49:29 +0430

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,42 @@
-var CACHE_NAME = '20200426215953';
-
-self.addEventListener('install',event => {
-  event.waitUntil(caches.open(CACHE_NAME)
-  .then(cache => cache.addAll([
+var CACHE_NAME = 'ehsanchehri';
+var urlsToCache = [
     '/',
     '/index.html'
-  ]))
-);
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener('fetch',event => {
-  event.respondWith(
-    caches.match(event.request)
-    .then(response => {
-      if (response) return response;
-
-      var fetchRequest = event.request.clone();
-
-      return fetch(fetchRequest).then(response => {
-          if (!response || response.status != 200 || response.type !== 'basic')
-            return response;
-
-          var responseToCache = response.clone();
-          caches.open(CACHE_NAME).then(cache =>
-            cache.put(event.request, responseToCache)
-          );
-          return response;
-        }).catch(() => caches.match('/'))
-    }));
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+            )
+    );
 });
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(CACHE_NAME) {
-      return Promise.all(
-        cacheNames.filter(function(CACHE_NAME) {
-        }).map(function(CACHE_NAME) {
-          return caches.delete(CACHE_NAME);
+self.addEventListener('activate', event => {
+    var cacheWhitelist = ['wms-pwa'];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
-      );
-    })
-  );
+    );
 });
